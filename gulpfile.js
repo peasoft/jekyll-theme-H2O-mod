03373e0f4008c4a5f0aa493417d6fd734bb15ac2
peasoft <luliucheng100@outlook.com>
2023-03-04 22:31:20 +0800

    Update to new code syntax

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,28 @@ var gulp = require('gulp'),
     cleanCSS = require('gulp-clean-css'), // 压缩css文件
     rename = require('gulp-rename'); // 文件重命名
 
-gulp.task('scripts', function(){
+gulp.task('scripts', function(done){
     gulp.src('dev/js/index.js')
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('assets/js'))
+        .pipe(gulp.dest('assets/js'));
+    done();
 });
 
-gulp.task('sass', function(){
+gulp.task('sass', function(done){
     gulp.src('dev/sass/app.scss')
         .pipe(sass())
         .pipe(gulp.dest('dev/sass'))
         .pipe(cleanCSS())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('assets/css'));
+    done();
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function(done){
     gulp.watch('dev/sass/*.scss', ['sass']);
     gulp.watch('dev/js/*.js', ['scripts']);
+    done();
 });
 
 gulp.task('default', gulp.series('scripts', 'sass', 'watch'));
